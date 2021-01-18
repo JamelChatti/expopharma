d64c0a04ae257f0fe364683805ef0c83dca94873
JamelChatti <chatti261@gmail.com>
2021-01-18 19:59:25 +0100

    Expanded

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -17,8 +17,137 @@ class _CategoriesState extends State<Categories> {
             centerTitle: true,
           ),
           drawer: MyDrawer(),
-          body: Container(
-            child: Text('Bonjour'),
+          body: GridView(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            children: <Widget>[
+              Container(
+                  child: Card(
+                child: Column(
+                  children: <Widget>[
+                   Expanded( child:Image.asset('images/labo/avene.png',fit:BoxFit.fill)),
+                    Container(child: Text('',style: TextStyle(fontSize: 20),))
+                  ],
+                ),
+              )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/acm.png',fit:BoxFit.fill)),
+                        Container(child: Text('',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/bioderma.jfif',fit:BoxFit.fill)),
+                        Container(child: Text('',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/bo.jfif',fit:BoxFit.fill)),
+                        Container(child: Text('',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/eye.png',fit:BoxFit.fill)),
+                        Container(child: Text('',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/filor.png',fit:BoxFit.fill)),
+                        Container(child: Text('FILORGA',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/lrp.png',fit:BoxFit.fill)),
+                        Container(child: Text('LA ROCHE POSAY',textAlign:TextAlign.center,style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/nature.jpg',fit:BoxFit.fill)),
+                        Container(child: Text('NATURE LAB',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/nore.jfif',fit:BoxFit.fill)),
+                        Container(child: Text('NOREVA',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/nux.jfif',fit:BoxFit.fill)),
+                        Container(child: Text('NUXE',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/pf.png',fit:BoxFit.fill)),
+                        Container(child: Text('',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/svr.png',fit:BoxFit.fill)),
+                        Container(child: Text('SVR',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/uri.jpg',fit:BoxFit.fill)),
+                        Container(child: Text('URIAGE',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+              Container(
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        Expanded( child:Image.asset('images/labo/vichy.jpg',fit:BoxFit.fill)),
+                        Container(child: Text('VICHY',style: TextStyle(fontSize: 20),))
+                      ],
+                    ),
+                  )),
+            ],
           ),
         ));
   }
