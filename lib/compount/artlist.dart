fc58a139f55239726a422689d1c0a0281ea49004
JamelChatti <chatti261@gmail.com>
2021-01-19 14:47:42 +0100

    Management file

---
+++ lib/compount/artlist.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+class ArtList extends StatelessWidget {
+  final nom;
+  final forme;
+  final indication;
+  final proprietes;
+  final utilisation;
+  final contenance;
+  final prix;
+
+  ArtList(
+      {this.nom, this.forme, this.prix, this.indication, this.proprietes, this.contenance, this.utilisation,});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        height: 200,
+        width: 100,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Image.asset('images/labo/avene/avecmi.jfif'),
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                      height: 200,
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.topLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Text(
+                                'Designation: ',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  nom,
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Colors.blueGrey,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          Row(
+                            children: [
+                              Text(
+                                'Forme: ',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                forme,
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Indication: ',
+
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                indication,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          Row(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: [
+
+                              Text(
+                                'Proprités: ',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+
+                              Column(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    proprietes,
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Sans parfum',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children:<Widget> [
+                              Text(
+                                'Contenance: ',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                contenance,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+
+
+                          Row(
+                            children: [Text(
+                              'Prix :',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                              Container(
+                                margin: EdgeInsets.only(top: 2),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.red)),
+                                padding: EdgeInsets.all(3),
+                                child: Text(
+                                  prix,
+                                  style: TextStyle(
+                                    color: Colors.green,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 5,),
+                          Text(
+                            'Pour plus d\'information taper sur l\'image',
+                            style: TextStyle(
+                                color: Colors.orange, fontSize: 10),
+                          )
+                        ],
+                      )))
+              //Expended التمدد
+            ],
+          ),
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+}
--- lib/pages/avene.dart
+++ lib/pages/avene.dart
@@ -4,30 +4,33 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:expopharma/compount/artlist.dart';
 class Avene extends StatefulWidget {
   @override
   _AveneState createState() => _AveneState();
 }
 
 class _AveneState extends State<Avene> {
-  var articlelist = [{
-     'nom':'Creme minérale spf50+',
-    'forme':'Créme',
-    'indication':'écran solaire',
-    'utilisation':'renouvelez fréquemment l’application',
-    'proprietes':'trés résistant à l\'eau',
-    'prix':'48500 dt'
+  var artlist = [{
+    'nom': 'Creme minérale spf50+',
+    'forme': 'Créme',
+    'indication': 'écran solaire',
+    'utilisation': 'renouvelez fréquemment l’application',
+    'proprietes': 'trés résistant à l\'eau',
+    'contenance': '50ml',
+    'prix': '48500 dt'
   },
     {
-      'nom':'Cleanance gel nettoyant ',
-      'forme':'Gel',
-      'indication':'Bouton et imperfection',
-      'utilisation':'une à deux fois par jour',
-      'proprietes':'nettoyant matifiant apaisant',
-      'prix':'30300 dt'
+      'nom': 'Cleanance gel nettoyant ',
+      'forme': 'Gel',
+      'indication': 'Bouton et imperfection',
+      'utilisation': 'une à deux fois par jour',
+      'proprietes': 'nettoyant matifiant apaisant',
+      'contenance': '200ml',
+      'prix': '30300 dt'
     }
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,194 +39,22 @@ class _AveneState extends State<Avene> {
           centerTitle: true,
         ),
         body: ListView.builder(
-          itemCount: articlelist.length,
-          itemBuilder: (context,i){
-            return//debut de la liste
-              InkWell(
-                child: Container(
-                  height: 200,
-                  width: 100,
-                  child: Card(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: Image.asset('images/labo/avene/avecmi.jfif'),
-                        ),
-                        Expanded(
-                            flex: 2,
-                            child: Container(
-                                height: 200,
-                                padding: EdgeInsets.all(10),
-                                alignment: Alignment.topLeft,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Row(
-                                      children: [
-                                        Text(
-                                          'Designation: ',
-                                          style: TextStyle(
-                                            fontSize: 10,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: Text(
-                                            articlelist[i]['nom'],
-                                            textAlign: TextAlign.start,
-                                            style: TextStyle(
-                                              color: Colors.blueGrey,
-                                              fontSize: 10,
-                                              fontWeight: FontWeight.w700,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+          itemCount: artlist.length,
+          itemBuilder: (context, i) {
+            return ArtList(nom: artlist[i]['nom'],
+                forme: artlist[i]['forme'],
+                indication: artlist[i]['indication'],
+                proprietes: artlist[i]['proprietes'],
+                utilisation: artlist[i]['utilisation'],
+                contenance: artlist[i]['contenance'],
+                prix: artlist[i]['prix']); //debut de la liste
 
-                                        Row(
-                                          children: [
-                                            Text(
-                                              'Forme: ',
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                            Text(
-                                              articlelist[i]['forme'],
-                                              textAlign: TextAlign.start,
-                                              style: TextStyle(
-                                                color: Colors.blueGrey,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        Row(
-                                          children: [
-                                            Text(
-                                              '    Indication: ',
-                                              textAlign: TextAlign.end,
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                            Text(
-                                              articlelist[i]['indication'],
-                                              style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-
-                                    Row(
-                                      crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                      children: [
-
-                                        Text(
-                                          'Proprités: ',
-                                          style: TextStyle(
-                                            fontSize: 10,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-
-                                        Column(
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            Text(
-                                              articlelist[i]['proprietes'],
-                                              style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                            Text(
-                                              'Sans parfum',
-                                              style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                            Text(
-                                              'Très résistant à l’eau',
-                                              textAlign: TextAlign.end,
-                                              style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                            Text(
-                                              'Très large protection ',
-                                              textAlign: TextAlign.end,
-                                              style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [Text(
-                                        'Prix :',
-                                        style: TextStyle(
-                                          color: Colors.green,
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                        Container(
-                                          margin: EdgeInsets.only(top: 2),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(color: Colors.red)),
-                                          padding: EdgeInsets.all(3),
-                                          child: Text(
-                                            articlelist[i]['prix'],
-                                            style: TextStyle(
-                                              color: Colors.green,
-                                              fontSize: 13,
-                                              fontWeight: FontWeight.w700,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(height: 5,),
-                                    Text(
-                                      'Pour plus d\'information taper sur l\'image',
-                                      style: TextStyle(
-                                          color: Colors.orange, fontSize: 10),
-                                    )
-                                  ],
-                                )))
-                        //Expended التمدد
-                      ],
-                    ),
-                  ),
-                ),
-                onTap: () {},
-              );
           },
 
 
-
-            //fin de la liste
+          //fin de la liste
 
         ));
   }
 }
+
